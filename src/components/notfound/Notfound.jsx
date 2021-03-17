60f33d234ab7ef2e0156692832183c32cdc365ed
Jérôme Guèrin <guerinjerome@outlook.fr>
2021-03-17 19:38:03 +0100

    ADD: Notfound page finished

--- src/components/notfound/Notfound.jsx
+++ src/components/notfound/Notfound.jsx
@@ -1,7 +1,16 @@
 import React from "react";
+import styles from "./Notfound.module.css";
 
 const Notfound = () => {
-    return <div></div>
+    return <div className={styles.background}>
+        <h2 className={styles.title}>
+            <span className={styles.green}>4</span>
+            <span className={styles.gradient}>0</span>
+            <span className={styles.white}>4</span>
+        </h2>
+        <h2 className={styles.subtitle}>Error</h2>
+        <h2 className={styles.explain}>Vérifiez l'URL saisie, il est fort probable que vous vous soyez trompé.</h2>
+    </div>
 }
 
 export default Notfound;
