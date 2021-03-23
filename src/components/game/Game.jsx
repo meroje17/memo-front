18698e8c8b5a1d3f6cf7a16c400e307b9777e091
Jérôme Guèrin <guerinjerome@outlook.fr>
2021-03-23 15:34:20 +0100

    FIX: remove old API

--- src/components/game/Game.jsx
+++ src/components/game/Game.jsx
@@ -3,35 +3,13 @@ import styles from "./Game.module.css";
 import { Pane, Avatar, Pill } from "evergreen-ui";
 import { useHistory } from "react-router-dom";
 
-const dataTest = [
-    {
-        username: "Ted",
-        score: 42000
-    },
-    {
-        username: "Nul",
-        score: 1
-    }
-];
-
 const Game = ({ user, launchAnimation }) => {
   const [score, setScore] = useState(0);
-  const [bestPlayers, setBestPlayers] = useState(dataTest);
   let history = useHistory();
 
   useEffect(() => {
     if (!user || user === "") history.push("/");
   }, [history, user]);
-
-  useEffect(() => {
-      // GET (pensez à créer le GET)
-  }, []);
-
-  /*const ranked = bestPlayers.map((player) => (
-    <p key={player.key}>
-      <span className={styles.rank}>{player.score}</span> - {player.username}
-    </p>
-  ));*/
 
   return (
     <div>
@@ -42,9 +20,6 @@ const Game = ({ user, launchAnimation }) => {
             {score}
           </Pill>
         </Pane>
-      </div>
-      <div className={styles.ranking}>
-        <h2 className={styles.rankingTitle}>CLASSEMENT</h2>
       </div>
     </div>
   );
